3b4ba4fbc95a8bb074631270a6d9da8cae4984f0
1000073584 <1000073584@hexaware.com>
2023-02-14 07:29:44 +0000

    push code

---
+++ springBootMSSQL/src/main/java/com/springBootMSSQL/exception/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package com.springBootMSSQL.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+	public EntityNotFoundException(String exception) {
+		super(exception);
+	}
+}
